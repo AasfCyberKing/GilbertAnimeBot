99b90f2d2e5b7916e725cbf017c4941b9f1a799a
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-11-04 22:11:09 +0530

    Update decorators.py

--- GilbertAnimeBot/modules/helper_funcs/decorators.py
+++ GilbertAnimeBot/modules/helper_funcs/decorators.py
@@ -1,7 +1,7 @@
-from Gilbertanimebot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
+from GilbertAnimeBot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
 from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler, InlineQueryHandler
 from telegram.ext.filters import BaseFilter
-from Gilbertanimebot import dispatcher as d, log
+from GilbertAnimeBot import dispatcher as d, log
 from typing import Optional, Union, List
 
 
