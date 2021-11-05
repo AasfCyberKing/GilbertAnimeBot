f047466917d2772157c757abe408b723a74a40d5
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-11-05 08:43:03 +0530

    Ooo

--- GilbertAnimeBot/__init__.py
+++ GilbertAnimeBot/__init__.py
@@ -84,6 +84,7 @@ if ENV:
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     CASH_API_KEY = os.environ.get('CASH_API_KEY', None)
     TIME_API_KEY = os.environ.get('TIME_API_KEY', None)
+    OPENWEATHERMAP_ID = os.environ.get('OPENWEATHERMAP_ID', None)
     AI_API_KEY = os.environ.get('AI_API_KEY', None)
     WALL_API = os.environ.get('WALL_API', None)
     REM_BG_API_KEY = os.environ.get('REM_BG_API_KEY', None)
@@ -162,6 +163,7 @@ else:
     ALLOW_EXCL = Config.ALLOW_EXCL
     CASH_API_KEY = Config.CASH_API_KEY
     TIME_API_KEY = Config.TIME_API_KEY
+    OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
     AI_API_KEY = Config.AI_API_KEY
     WALL_API = Config.WALL_API
     REM_BG_API_KEY = Config.REM_BG_API_KEY
